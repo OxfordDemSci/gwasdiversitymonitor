77df27ed78ccc3788d6c79e5b9b214c3f1f7cb9c
vallerrr <36286934+vallerrr@users.noreply.github.com>
2021-02-21 12:07:32 +0000

    Final fix of the GA Keys

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ app.config.from_object('config')
 #specify the Google Analytics key here
 app.config["GA_KEY"]=''
 
-#specify the Google Analytics key here
-app.config["GA_KEY"]=''
 from app import routes
 
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
