189ba6138d9c3b41a55dcb367f853204a616396b
jiani <valler4044@gmail.com>
2021-02-21 11:49:06 +0000

    added the Google Analytics key specifying sentence

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
+#specify the Google Analytics key here
+app.config["GA_KEY"]=''
 from app import routes
 
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
