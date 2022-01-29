40c774e344a82227405eb51eb6de599e84ee4ba1
crahal <charlierahal@gmail.com>
2022-01-29 00:15:21 +0000

    Revert back changes to [22d85c9](https://github.com/OxfordDemSci/gwasdiversitymonitor/commit/22d85c9ea8b1ad5615b70cb4871ee8505319b14f), partly because of #17, but also because of how objects are passed in generate_data.main().

--- gwasdiversitymonitor.py
+++ gwasdiversitymonitor.py
@@ -6,9 +6,6 @@ import sys
 if __name__ == '__main__':
     required_data_file = 'app/data/toplot/chloroMap.json'
     if not os.path.exists(required_data_file):
-        sys.stderr.write('Missing required data files. Attempting to download them now\n')
-        generate_data.main()
-    if not os.path.exists(required_data_file):
-        sys.stderr.write('Data download failed. Aborting\n')
+        sys.stderr.write('Missing required data files. Please run python3 ./app/generate_data.py!\n')
         sys.exit(1)
     app.run(host='0.0.0.0', debug=True)
