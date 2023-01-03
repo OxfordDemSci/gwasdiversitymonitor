f88303b9deccee2973e712f0afc98b5dfd1e50f0
charlie <charlierahal@gmail.com>
2023-01-03 14:59:37 +0800

    strip_extension set to False for newer sass versions

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,5 @@ app.config["GA_KEY"]=''
 from app import routes
 
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    'app': ('static/sass', 'static/css', '/static/css')
+    'app': ('static/sass', 'static/css', '/static/css', False)
 })
