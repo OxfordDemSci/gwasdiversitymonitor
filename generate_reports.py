e51fd5aac42c3ba5ddb2f5f7e3bedbc689201df4
charlie <charlierahal@gmail.com>
2023-01-04 17:14:52 +0800

    Embedding figure into template

--- generate_reports.py
+++ generate_reports.py
@@ -1,14 +1,18 @@
 import os
 import PyPDF2
 import shutil
+import matplotlib.pyplot as plt
+import pdflatex
 from numpy import loadtxt
 from tqdm import tqdm
-from pylatex import Document, Section, Subsection,\
-    Command, Head, Foot, PageStyle, LineBreak,\
-    MiniPage, LargeText, MediumText, SmallText
+from matplotlib.gridspec import GridSpec
 from pylatex.utils import escape_latex, italic, NoEscape, bold
 from pylatex import Hyperref, Package
-import pdflatex
+# @TODO some redundancy here
+from pylatex import Document, Section, Subsection,\
+    Command, Head, Foot, PageStyle, LineBreak,\
+    MiniPage, LargeText, MediumText, SmallText,\
+    Figure, Subsubsection
 
 def hyperlink(url, text):
     text = escape_latex(text)
@@ -52,34 +56,29 @@ def fill_document(doc, agency):
                                 options=['colorlinks=true',
                                          'linkcolor=blue',
                                          'urlcolor=Blue']))
-    with doc.create(Subsection('General Summary Statistics',
-                               numbering=False)):
-        doc.append('The ')
-        doc.append(hyperlink("https://www.gwasdiversitymonitor.com",
-                             "GWAS Diversity Monitor "))
-        doc.append(' believes that the ')
-        doc.append(hyperlink("https://www.ebi.ac.uk/gwas/",
-                             "NHGRI-EBI GWAS Catalog "))
-        doc.append(' has cached X authorships across Y studies '
-                   'in Z papers which are funded by ' + agency)
-        doc.append('. This represents A, B and C of entire Catalog '
-                   'respectively.')
-        doc.append('The earliest study found in the Catalog which '
-                   'occured at DDDD, and the latest was at DDDD. ')
-        doc.append('These studies involved a total of Y participants. ')
-        doc.append('The most commonly studied Disease/Trait was X,'
-                   'and the most commonly studied parent term was Y.')
+#   with doc.create(Subsection('General Summary Statistics',
+#                              numbering=False)):
+    doc.append('The ')
+    doc.append(hyperlink("https://www.gwasdiversitymonitor.com",
+                         "GWAS Diversity Monitor "))
+    doc.append(' believes that the ')
+    doc.append(hyperlink("https://www.ebi.ac.uk/gwas/",
+                         "NHGRI-EBI GWAS Catalog "))
+    doc.append(' has cached X authorships across Y studies '
+               'in Z papers which are funded by ' + agency)
+    doc.append('. This represents A, B and C of entire Catalog '
+               'respectively.')
+    doc.append('The earliest study found in the Catalog which '
+               'occured at DDDD, and the latest was at DDDD. ')
+    doc.append('These studies involved a total of Y participants. ')
+    doc.append('The most commonly studied Disease/Trait was X,'
+               'and the most commonly studied parent term was Y.')
 
+    with doc.create(Figure(position = 'h!')) as fig:
+        fig.add_image('figure.pdf', width='500px')
+#        fig.add_caption('My caption here')
 
-    with doc.create(Subsection('Diversity Statistics',
-                               numbering=False)):
-        doc.append('The total')
-
-    with doc.create(Subsection('Insert Figure Here',
-                               numbering=False)):
-        doc.append('stats')
-
-    with doc.create(Subsection('About the GWAS Diversity Monitor',
+    with doc.create(Subsubsection('About the GWAS Diversity Monitor',
                                numbering=False)):
         doc.append('The GWAS Diversity Monitor is maintained by the ')
         doc.append(hyperlink("https://www.demographicscience.ox.ac.uk/",
@@ -132,7 +131,16 @@ def execute_tex(reports_path, data_path, agency_list):
             watermark(path, data_path)
 
 def build_figures(agency_list, data_path, reports_path):
-    pass
+    for agency in agency_list:
+        gs = GridSpec(2, 2)
+        fig = plt.figure(figsize=(8, 4))
+        ax1 = fig.add_subplot(gs[0, 0])
+        ax2 = fig.add_subplot(gs[0, 1])
+        ax3 = fig.add_subplot(gs[1, :])
+        plt.tight_layout()
+        figpath = os.path.join(reports_path, agency, 'figure.pdf')
+        plt.savefig(figpath)
+
 def generate_reports(data_path, reports_path):
     agency_list = get_agency_list(data_path)
     build_figures(agency_list, data_path, reports_path)
