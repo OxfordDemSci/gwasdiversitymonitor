80b54756f9e926d112f6aa35aa8b3f4fffe7f0e9
gi-liam <liam@global-initiative.com>
2023-02-01 14:56:22 +0000

    Added flask caching and GZIP compression

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import sys
 from flask import Flask
 from sassutils.wsgi import SassMiddleware
 
+# Ensure that the config is always imported when starting the application
+sys.path.append('/home/gi-liam/Documents/Code/gwasdiversitymonitor/deploy')
+
 app = Flask(__name__)
 app.config.from_object('config')
+
 
 #specify the Google Analytics key here
 app.config["GA_KEY"]=''
