aa434b2264ed0c695ffe8708e53f87ab5966ed28
gi-liam <liam@global-initiative.com>
2023-02-02 12:51:02 +0000

    Updated config path to be relative

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
+import os
 import sys
+
 from flask import Flask
 from sassutils.wsgi import SassMiddleware
 
 # Ensure that the config is always imported when starting the application
-sys.path.append('/home/gi-liam/Documents/Code/gwasdiversitymonitor/deploy')
+config_path = os.getcwd() + '/deploy'
+sys.path.append(config_path)
+
 
 app = Flask(__name__)
 app.config.from_object('config')
