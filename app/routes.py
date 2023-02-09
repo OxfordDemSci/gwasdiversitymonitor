2191a2a30c81d2a473ceed3eb32acb2479aac5ab
gi-liam <liam@global-initiative.com>
2023-02-09 10:59:41 +0000

    Updated funders & CSV endpoint

--- app/routes.py
+++ app/routes.py
@@ -85,9 +85,9 @@ def additional():
 def getCSV(filename):
 
     if filename == "heatmap" or filename == "timeseries" or filename == "gwasdiversitymonitor_download":
-        return send_file(os.path.join('..', 'data', 'todownload', filename + '.zip'))
+        return send_file(os.path.join(os.getcwd(), 'data', 'todownload', filename + '.zip'))
 
-    with open(os.path.join('..', 'data', 'toplot', filename + '.csv')) as fp:
+    with open(os.path.join(os.getcwd(), 'data', 'toplot', filename + '.csv')) as fp:
         csv = fp.read()
 
     return Response(
@@ -127,37 +127,14 @@ def getFilterTraits():
 @app.route("/api/funders")
 def getFilterFunders():
     funders_list = []
-    # Converted the funders_cleaner.txt to json for easier traversal
-    with open("data/support/funder_cleaner.json") as file:
-        data = json.load(file)
-        # Sorting the data in the backend before sending returning response
-        sorted_grouping_list = sorted([grouping for grouping in set(data.values())])
-        grouping_dict = {grouping: [] for grouping in sorted_grouping_list}
-
-        for funder, grouping in data.items():
-            grouping_dict[grouping].append(funder)
-
-        sorted_group_dict = {}
-        for grouping in grouping_dict.keys():
-            sorted_group_dict[grouping] = sorted(grouping_dict[grouping])
-
-        for group_index, grouping in enumerate(sorted_group_dict.keys(), start=1):
-            text = grouping
-            # Avoids duplicate element ids
-            if group_index < 11:
-                current_group = {"id": group_index, "text": text}
-            else:
-                current_group = {"id": (group_index * 10), "text": text}
-            current_group["inc"] = []
-            funders_list.append(current_group)
-            # Current group will be the last element of the list as it was just appended
-            for funder_index, funder in enumerate(sorted_group_dict[grouping], start=1):
-                # "id" is used to determine tree hierachy in funder dropdown
-                if group_index < 11:
-                    element_tree_id = (group_index * 10) + funder_index
-                else:
-                    element_tree_id = (group_index * 100) + funder_index
-                current_group["inc"].append({"id": element_tree_id, "text": f"{funder}"})
-
-    json_object = json.dumps({"data": funders_list}, sort_keys=False)
+    file_path = os.getcwd() + "/data/pubmed/agency_list.txt"
+    with open(file_path) as file:
+        data = file.readline()
+        sorted_funders_name_list = sorted([x for x in data.split(",") if len(x) > 0])
+        funders_list = []
+        for n, funder_name in enumerate(sorted_funders_name_list):
+            funder_dictionary = {"id": n, "text": funder_name, "inc": []}
+            funders_list.append(funder_dictionary)
+    print(funders_list)
+    json_object = json.dumps({"data": funders_list})
     return (json_object)
