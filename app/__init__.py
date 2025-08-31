00ae37beef239c5a529aaf4b40fe025bbb739946
crahal <charlierahal@gmail.com>
2025-08-31 15:06:05 +0100

    fix strip_extension in sasutils warning

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from sassutils.wsgi import SassMiddleware
 app = Flask(__name__)
 app.config.from_object('config')
 
-#specify the Google Analytics key here
-app.config["GA_KEY"]=''
+# specify the Google Analytics key here
+app.config["GA_KEY"] = ''
 
 from app import routes
 
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    'app': ('static/sass', 'static/css', '/static/css')
+    'app': ('static/sass', 'static/css', '/static/css', False)  # last arg = strip_extension
 })
